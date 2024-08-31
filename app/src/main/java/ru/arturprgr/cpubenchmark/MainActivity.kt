79d7364015b129edfc85f93e0e5125abf257f9c2
arturaff <arturars2022@gmail.com>
2024-08-31 14:41:54 +0500

    Обновлена логика проведения теста и при его окончании вибрирует телефон

--- app/src/main/java/ru/arturprgr/cpubenchmark/MainActivity.kt
+++ app/src/main/java/ru/arturprgr/cpubenchmark/MainActivity.kt
@@ -1,10 +1,14 @@
 package ru.arturprgr.cpubenchmark
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
 import android.os.SystemClock
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -14,11 +18,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import ru.arturprgr.cpubenchmark.databinding.ActivityMainBinding
+import java.text.DecimalFormat
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var thread: Thread? = Thread()
+    private var thread: Thread? = null
     private var isWorked: Boolean = false
 
     @SuppressLint("SetTextI18n", "NewApi")
@@ -34,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.apply {
-            chronometer.base = SystemClock.elapsedRealtime() + 30000
+            resetChronometer()
 
             buttonInfo.setOnClickListener {
                 viewAlertDialog(
@@ -51,55 +56,62 @@ class MainActivity : AppCompatActivity() {
                     resources.getString(R.string.information_2),
                     resources.getString(R.string.start)
                 ) {
-                    chronometer.base = SystemClock.elapsedRealtime() + 30000
                     val handler = @SuppressLint("HandlerLeak") object : Handler() {
                         override fun handleMessage(msg: Message) {
                             super.handleMessage(msg)
-                            if (!(msg.data.getBoolean("isWorked"))) {
-                                thread = null
-                                isWorked = !isWorked
-                                chronometer.stop()
-                                buttonStartStopTest.text =
-                                    resources.getString(R.string.start_test)
-                            } else textResult.text = "${msg.data.getInt("result")}"
+                            thread = null
+                            isWorked = !isWorked
+                            textResult.text = "${msg.data.getString("result")}"
+                            chronometer.stop()
+                            buttonStartStopTest.text = resources.getString(R.string.start_test)
+                            val vibrator =
+                                this@MainActivity.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                vibrator.vibrate(
+                                    VibrationEffect.createOneShot(
+                                        500L,
+                                        VibrationEffect.DEFAULT_AMPLITUDE
+                                    )
+                                )
+                            } else {
+                                vibrator.vibrate(500L)
+                            }
                         }
                     }
                     thread = Thread {
-                        for (scores in 1..2147483647) {
-                            try {
-                                if (chronometer.base <= SystemClock.elapsedRealtime()) {
-                                    val bundle = Bundle()
-                                    val message = handler.obtainMessage()
-                                    bundle.putInt("result", scores)
-                                    bundle.putBoolean("isWorked", false)
-                                    message.data = bundle
-                                    handler.sendMessage(message)
-                                    break
-                                } else {
-                                    val bundle = Bundle()
-                                    val message = handler.obtainMessage()
-                                    bundle.putInt("result", scores)
-                                    bundle.putBoolean("isWorked", true)
-                                    message.data = bundle
-                                    handler.sendMessage(message)
-                                }
-                            } catch (_: NullPointerException) {
+                        for (scores in 0..2147483647) {
+                            if (chronometer.base <= SystemClock.elapsedRealtime()) {
+                                val bundle = Bundle()
+                                val msg = handler.obtainMessage()
+                                bundle.putString(
+                                    "result",
+                                    DecimalFormat("###,###.##").format(scores)
+                                )
+                                msg.data = bundle
+                                handler.sendMessage(msg)
                                 break
                             }
                         }
                     }
+                    textResult.text = "Процесс..."
                     thread!!.start()
-                    chronometer.base = SystemClock.elapsedRealtime() + 30000
+                    resetChronometer()
                     chronometer.start()
                     buttonStartStopTest.text = resources.getString(R.string.stop_test)
                 }
                 else {
+                    textResult.text = "0"
                     thread = null
+                    resetChronometer()
                     chronometer.stop()
                     buttonStartStopTest.text = resources.getString(R.string.start_test)
                 }
             }
         }
+    }
+
+    private fun resetChronometer() {
+        binding.chronometer.base = SystemClock.elapsedRealtime() + 60000
     }
 
     private fun viewAlertDialog(
@@ -108,20 +120,20 @@ class MainActivity : AppCompatActivity() {
         buttonText: String,
         onClick: () -> Unit,
     ) = binding.apply {
-            val textView = TextView(this@MainActivity)
-            textView.text = message
-            AlertDialog.Builder(this@MainActivity)
-                .setTitle(title)
-                .setView(textView)
-                .setPositiveButton(buttonText) { _, _ ->
-                    onClick()
-                }
-                .create()
-                .show()
-            textView.updateLayoutParams<FrameLayout.LayoutParams> {
-                this.topMargin = 32
-                this.leftMargin = 70
-                this.rightMargin = 70
+        val textView = TextView(this@MainActivity)
+        textView.text = message
+        AlertDialog.Builder(this@MainActivity)
+            .setTitle(title)
+            .setView(textView)
+            .setPositiveButton(buttonText) { _, _ ->
+                onClick()
             }
+            .create()
+            .show()
+        textView.updateLayoutParams<FrameLayout.LayoutParams> {
+            this.topMargin = 32
+            this.leftMargin = 70
+            this.rightMargin = 70
         }
+    }
 }
